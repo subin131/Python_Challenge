fd9006a87610754c949ccc24347e13b91ae22c94
satyal <satyalsubin33@gmail.com>
2022-10-21 13:08:58 +0545

    simple profit loss calculation

---
+++ python/ProfiltLoss.py
@@ -0,0 +1,8 @@
+#taking two varibale for cost price and selling price
+from cmath import cos
+
+
+cost_price=int(input("Enter the cost price: "))
+selling_price=int(input("Enter the selling price: "))
+result=selling_price-cost_price
+print(result)
