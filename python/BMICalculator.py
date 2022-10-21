9ed00293d33b4adf65a928a136591529fe14eeac
satyal <satyalsubin33@gmail.com>
2022-10-21 13:07:35 +0545

    bmi calculator using python

---
+++ python/BMICalculator.py
@@ -0,0 +1,11 @@
+# take float input for weight
+weight=float(input("Enter the weight in kg: "))
+
+# take float input for height
+height=float(input("Enter the height in meter: "))
+
+# calculate BMI
+BMI=weight/height**2
+
+# print the calculated BMI
+print(BMI)
